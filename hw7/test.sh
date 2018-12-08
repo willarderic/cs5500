87d5d8c4a6488b8855a9702d0477361f1ba2ef87
Willard <emwwwc@rc07ucs213.managed.mst.edu>
2018-12-07 19:55:36 -0600

    done

--- hw7/test.sh
+++ hw7/test.sh
@@ -10,33 +10,27 @@ flex willardorth.l
 check
 bison willardorth.y
 check
-g++ willardorth.tab.c -o compiler
+g++ willardorth.tab.c -o miplc
 check
-echo "Flexing, bisoning, compiling VM..."
-flex oal.l
-check
-bison oal.y
-check
-g++ oal.tab.c -o vm
-check
-echo "Running Tests..."
 
 for file in `ls inputs`; do
   echo -n "Running $file..."
-  ./compiler inputs/$file > temp.txt
-  ./vm temp.txt
-  diff --ignore-space-change --ignore-case --ignore-blank-lines temp.txt outputs/$file.out > diff.txt
-  if [ $? -eq 0 ]; then
-    echo -e "PASS"
-  else
+  ./miplc inputs/$file > tmp.s
+  as tmp.s -o tmp.o
+  ld tmp.o
+  ./a.out > temp.txt 2>&1
+  #diff --ignore-space-change --ignore-case --ignore-blank-lines temp.txt outputs/$file.out > diff.txt
+  #if [ $? -eq 0 ]; then
+  #  echo -e "PASS"
+  #else
     # If your file failed, uncomment the following lines
-    echo -e "FAIL"
-    echo -e "File Diff:"
-    cat diff.txt
-    #echo -e "\e[91mYour Output:\e[39m"
-    #cat temp.txt
-    #exit 1
-  fi
+  #  echo -e "FAIL"
+  #  echo -e "File Diff:"
+  #  cat diff.txt
+  #  echo -e "\e[91mYour Output:\e[39m"
+  #  cat temp.txt
+  #  exit 1
+  #fi
 done
 
-rm willarde.tab.c willarde_parser lex.yy.c temp.txt diff.txt
+rm willardorth.tab.c miplc lex.yy.c temp.txt diff.txt tmp.o tmp.s a.out
